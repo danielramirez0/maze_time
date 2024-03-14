eb2a3a91d5cbd2a32be3ea1d4c89ed1376002332
Daniel Ramirez <zoerunner@gmail.com>
2024-03-13 19:09:32 -0700

    Fix bug with positional arg

--- src/cell.py
+++ src/cell.py
@@ -1,8 +1,14 @@
-from graphics import Line, Point 
+from graphics import Line, Point
 
 
 class Cell:
-    def __init__(self, top_left_cords: tuple, bottom_right_cords: tuple, window = None):
+    def __init__(
+        self,
+        top_left_cords: tuple,
+        bottom_right_cords: tuple,
+        window = None,
+        visited: bool = False,
+    ):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -10,6 +16,7 @@ class Cell:
         self._top_left_cords = top_left_cords
         self._bottom_right_cords = bottom_right_cords
         self._window = window
+        self.visited = visited
 
     def draw(self):
         x1, y1 = self._top_left_cords
@@ -38,15 +45,15 @@ class Cell:
         else:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._window.draw_line(line, "white")
-    
+
     def get_center_point(self):
         x1, y1 = self._top_left_cords
         x2, y2 = self._bottom_right_cords
         return Point((x1 + x2) / 2, (y1 + y2) / 2)
-     
+
     def draw_move(self, to_cell, undo=False):
         if self._window is None:
-            return 
+            return
         center_start = self.get_center_point()
         center_end = to_cell.get_center_point()
         line = Line(center_start, center_end)
