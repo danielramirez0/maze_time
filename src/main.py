46587cb2b6a1a18a850e9ffb10531e8a160936c3
Daniel Ramirez <zoerunner@gmail.com>
2024-03-13 19:11:07 -0700

    Fix cell initialization from list of rows, to list of columns for consistency in the rest of the code base. Add wall breaker and update main[

--- src/main.py
+++ src/main.py
@@ -4,60 +4,19 @@ from maze import Maze
 
 
 def main():
-    window = Window(800, 600)
 
-    # point_a = Point(100, 100)
-    # point_b = Point(200, 200)
-    # line = Line(point_a, point_b)
-    # window.draw_line(line, "black")
-    # point_a.x = 300
-    # point_a.y = 300
-    # point_b.x = 400
-    # point_b.y = 400
-    # next_line = Line(point_a, point_b)
-    # window.draw_line(next_line, "red")
-    # cell = Cell((0, 100), (0, 100), window)
-    # cell.draw()
-    # c = Cell(window, (10, 10), (50, 50))
-    # c.has_left_wall = False
-    # c.draw()
-    # c = Cell(window, (100, 100), (150, 150))
-    # c.has_right_wall = False
-    # c.draw()
-    # c = Cell(window, (200, 200), (250, 250))
-    # c.has_bottom_wall = False
-    # c.draw()
-    # c = Cell(window, (300, 300), (350, 350))
-    # c.has_top_wall = False
-    # c.draw()
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_width = (screen_x - 2 * margin) / num_cols
+    cell_height = (screen_y - 2 * margin) / num_rows
+    window = Window(screen_x, screen_y)
 
-    # c1 = Cell(window, (50, 50), (100, 100))
-    # c1.has_right_wall = False
-    # c1.draw()
-
-    # c2 = Cell(window, (100, 50), (150, 100))
-    # c2.has_left_wall = False
-    # c2.has_bottom_wall = False
-    # c2.draw()
-
-    # c1.draw_move(c2)
-
-    # c3 = Cell(window, (100, 100), (150, 150))
-    # c3.has_top_wall = False
-    # c3.has_right_wall = False
-    # c3.draw()
-
-    # c2.draw_move(c3)
-
-    # c4 = Cell(window, (150, 100), (200, 150))
-    # c4.has_left_wall = False
-    # c4.draw()
-
-    # c3.draw_move(c4, True)
-
-    maze = Maze(10, 10, 5, 5, 100, 100, window)
-    maze._break_entrance_and_exit()
-
+    maze = Maze(
+        margin, margin, num_rows, num_cols, cell_width, cell_height, window, 10
+    )
 
     window.wait_for_close()
 
--- src/maze.py
+++ src/maze.py
@@ -1,3 +1,4 @@
+import random
 import time
 from cell import Cell
 
@@ -12,6 +13,7 @@ class Maze:
         cell_width,
         cell_height,
         window=None,
+        seed=None,
     ):
         self._cells = []
         self._x = x
@@ -21,13 +23,16 @@ class Maze:
         self._cell_width = cell_width
         self._cell_height = cell_height
         self._window = window
+        self.seed = random.seed(seed) if seed else None
 
         self._create_cells()
+        self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
 
     def _create_cells(self):
-        for i in range(self._num_rows):
+        for i in range(self._num_cols):
             column = []
-            for j in range(self._num_cols):
+            for j in range(self._num_rows):
                 top_left_cords = (
                     self._x + (i * self._cell_width),
                     self._y + (j * self._cell_height),
@@ -38,8 +43,8 @@ class Maze:
                 )
                 column.append(Cell(top_left_cords, bottom_right_cords, self._window))
             self._cells.append(column)
-        for i in range(self._num_rows):
-            for j in range(self._num_cols):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i: int, j: int):
@@ -52,10 +57,48 @@ class Maze:
         if self._window is None:
             return
         self._window.redraw()
-        time.sleep(0.05)
+        time.sleep(0.01)
 
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
         self._draw_cell(0, 0)
         self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            adjacent_options = self._get_adjacent_cells(i, j)
+            if len(adjacent_options) == 0:
+                self._draw_cell(i, j)
+                return
+
+            direction_index = random.randrange(len(adjacent_options))
+            next_index = adjacent_options[direction_index]
+
+            if next_index[0] == i + 1:
+                self._cells[i][j].has_right_wall = False
+                self._cells[i + 1][j].has_left_wall = False
+            if next_index[0] == i - 1:
+                self._cells[i][j].has_left_wall = False
+                self._cells[i - 1][j].has_right_wall = False
+            if next_index[1] == j + 1:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[i][j + 1].has_top_wall = False
+            if next_index[1] == j - 1:
+                self._cells[i][j].has_top_wall = False
+                self._cells[i][j - 1].has_bottom_wall = False
+
+            self._break_walls_r(next_index[0], next_index[1])
+
+    def _get_adjacent_cells(self, i, j):
+        adjacent_cells = []
+        if i > 0 and not self._cells[i - 1][j].visited:
+            adjacent_cells.append((i - 1, j))
+        if i < self._num_cols - 1 and not self._cells[i + 1][j].visited:
+            adjacent_cells.append((i + 1, j))
+        if j > 0 and not self._cells[i][j - 1].visited:
+            adjacent_cells.append((i, j - 1))
+        if j < self._num_rows - 1 and not self._cells[i][j + 1].visited:
+            adjacent_cells.append((i, j + 1))
+        return adjacent_cells
